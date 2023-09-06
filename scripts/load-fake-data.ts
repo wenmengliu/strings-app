212d367d6f4e5d671f28436be740ca0a06d5e9ef
Wenmeng Liu <crystallwm2011@gmail.com>
2023-09-05 20:54:58 -0700

    bcrypt

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,6 +1,7 @@
 import { Client } from "pg";
 import { faker } from "@faker-js/faker";
 import { loadEnvConfig } from "@next/env";
+import bcrypt from "bcrypt";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
@@ -20,9 +21,12 @@ async function loadFakeData(numUsers: number = 10) {
     await client.query("begin");
 
     for (let i = 0; i < numUsers; i++) {
+      const saltRounds = 10;
+      const hash = await bcrypt.hashSync("strings123", saltRounds);
+
       await client.query(
         "insert into users (username, password, avatar) values ($1,$2,$3)",
-        [faker.internet.userName(), "password", faker.image.avatar()]
+        [faker.internet.userName(), hash, faker.image.avatar()]
       );
     }
 
